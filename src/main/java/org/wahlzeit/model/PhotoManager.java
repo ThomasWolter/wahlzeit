a9f8ccb33f0289ff259af04fbbb18532827018bf
Thomas <tho.wolter@fau.de>
2020-11-23 00:54:16 +0100

    Added: CartoonCharPhotoManager + Set instance in PhotoManager

--- src/main/java/org/wahlzeit/model/PhotoManager.java
+++ src/main/java/org/wahlzeit/model/PhotoManager.java
@@ -35,7 +35,7 @@ public class PhotoManager extends ObjectManager {
 	/**
 	 * 
 	 */
-	protected static final PhotoManager instance = new PhotoManager();
+	protected static final PhotoManager instance = new CartoonCharPhotoManager();
 
 	/**
 	 * In-memory cache for photos
