1c95a6270e4a117cdd2e0eff78f4349ea87502f4
Thomas <tho.wolter@fau.de>
2020-11-23 00:53:18 +0100

    Added: CartoonCharPhotoFactory + Set instance in PhotoFactory

---
+++ src/main/java/org/wahlzeit/model/CartoonCharPhotoFactory.java
@@ -0,0 +1,27 @@
+package org.wahlzeit.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CartoonCharPhotoFactory extends PhotoFactory{
+
+    @Override
+    public CartoonCharPhoto createPhoto() {
+        return new CartoonCharPhoto();
+    }
+
+    @Override
+    public CartoonCharPhoto createPhoto(PhotoId id) {
+        return new CartoonCharPhoto(id);
+    }
+
+    @Override
+    public CartoonCharPhoto createPhoto(ResultSet rSet) throws SQLException {
+        return new CartoonCharPhoto(rSet);
+    }
+
+    public CartoonCharPhoto createPhoto(String name, String company, int debut) {
+        return new CartoonCharPhoto(name, company, debut);
+    }
+
+}
